e014e3189d9a1e2b05f69ba2a9de9810ffcc21f8
bheemreddysakethreddy <sakethbheem@gmail.com>
2026-02-16 13:33:49 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,13 @@ const cors = require("cors");
 // configuring the env file and can access the env keys
 require("dotenv").config();
 
+
 // importing the mongodb connection
 const mongodbConnection = require("./config/connection");
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
 const PORT = process.env.PORT;
 app.use(express.json());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
-);
+app.use(cors());
 //making products image folder as public
 app.use(express.static("productimages"));
 
@@ -22,7 +18,7 @@ mongodbConnection()
   .then(() =>
     app.listen(PORT, () => {
       console.log("Server running at port:", PORT);
-    }),
+    })
   )
   .catch((err) => console.log("failed to connect DataBase", err));
 
@@ -35,4 +31,4 @@ const adminRouter = require("./routers/admin");
 app.use("/", userRouter);
 app.use("/products", productRouter);
 app.use("/cart", cartRouter);
-app.use("/", adminRouter);
+app.use("/", adminRouter)
